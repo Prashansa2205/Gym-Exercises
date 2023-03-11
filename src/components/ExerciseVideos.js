87ad5eb08d265a8d19ad84d92b72ef0e87aa5f88
Prashansa Toppo <toppo.prashansa22@gmail.com>
2023-03-11 10:24:23 +0530

    Add Similar & Equipment Exercises

--- src/components/ExerciseVideos.js
+++ src/components/ExerciseVideos.js
@@ -1,22 +1,18 @@
 import React from 'react'
 import { Box, Stack, Typography } from '@mui/material';
 
+import Loader from './Loader';
+
 const ExerciseVideos = ({ exerciseVideos, name }) => {
 
-  if(!exerciseVideos.length) return 'Loading...'
+  if(!exerciseVideos.length) return <Loader />
 
   return (
     <Box
       sx ={{ marginTop: { lg:'200px', xs: '20px'}}} p="20px"
     >
       <Typography  variant='h3' mb="33px">
-        Watch 
-        <span
-          style={{ color:'#ff2625', textTransform: 'capitalize'}}
-        >
-          {name}
-        </span> 
-        exercise videos
+        Watch <span style={{ color:'#ff2625', textTransform: 'capitalize'}}> {name} </span> exercise videos
       </Typography>
       <Stack
         justifyContent="flex-start" flexWrap="wrap" alignItems="center"
--- src/components/SimilarExercises.js
+++ src/components/SimilarExercises.js
@@ -1,8 +1,37 @@
-import React from 'react'
+import React from 'react';
+import { Box, Typography, Stack } from '@mui/material';
 
-const SimilarExercises = () => {
+import HorizontalScrollbar from './HorizontalScrollbar';
+import Loader from './Loader';
+
+const SimilarExercises = ({ targetMuscleExercises, equipmentExercises }) => {
   return (
-    <div>SimilarExercises</div>
+    <Box 
+    sx={{ mt: {lg: '100px', xs:'0'}}}
+    >
+      <Typography variant='h3' mb={5}>
+        Exercise that target the same muscle group
+      </Typography>
+      <Stack
+        direction="row" sx={{ p:'2', position: 'relative' }}
+      >
+        {targetMuscleExercises.length ? 
+          <HorizontalScrollbar data={targetMuscleExercises}/>
+          : <Loader/>
+        }
+      </Stack>
+      <Typography variant='h3' mb={5}>
+        Exercise that target the same equipment group
+      </Typography>
+      <Stack
+        direction="row" sx={{ p:'2', position: 'relative' }}
+      >
+        {equipmentExercises.length ? 
+          <HorizontalScrollbar data={equipmentExercises}/>
+          : <Loader/>
+        }
+      </Stack>
+    </Box>
   )
 }
 
